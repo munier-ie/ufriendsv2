7607dcc1a99337e4db4ab6def25dab360e2251dc
munier-ie <munirisah2021@gmail.com>
2026-04-16 06:08:18 +0100

    deploy: add react import to necessary component

--- src/contexts/LandingContentContext.jsx
+++ src/contexts/LandingContentContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState, useCallback } from 'react';
+import React, { createContext, useContext, useState, useCallback } from 'react';
 import { loadLandingContent, saveLandingContent } from '../lib/landingContent';
 
 const LandingContentContext = createContext(null);
