4bcbc2a5f2e01d3ff4c50b81c78fa862a43b653c
munier-ie <munirisah2021@gmail.com>
2026-04-16 06:11:33 +0100

    fix: force automatic jsx transform

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+        react({
+            // Force the automatic JSX transform
+            jsxRuntime: 'automatic',
+        }),
+    ],
+    esbuild: {
+        // This ensures esbuild also uses the automatic transform
+        jsx: 'automatic',
+    },
     server: {
         proxy: {
             '/api': {
