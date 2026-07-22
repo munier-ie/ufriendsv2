bc7efdffa810f0671a7fa86a34bb40a2d82e2b65
munier-ie <munirisah2021@gmail.com>
2026-07-22 13:24:31 +0100

    fix: auto-start HTTP listener in api/index.js when run by PM2

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
 const app = require('./app');
 
+if (require.main === module) {
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => {
+        console.log(`API server listening on port ${port}`);
+    });
+}
+
 module.exports = app;
